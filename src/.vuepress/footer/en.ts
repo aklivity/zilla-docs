80dd5a786144d346a825746edf315914e5f1e7b7
Leonid Lukyanov <leonid.s.lukyanov@gmail.com>
2023-05-03 15:56:47 -0700

    Delete src/.vuepress/footer directory

--- src/.vuepress/footer/en.ts
+++
@@ -1,6 +0,0 @@
-import { footer } from "vuepress-theme-hope";
-
-export const enFooter = footer([
-	{ text: "aklivity", icon: "clock", link: "https://www.aklivity.io" },
-	{ text: "twitter", icon: "twitter", link: "https://www.twitter.com" }
-]);
