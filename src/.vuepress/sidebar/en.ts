05483937337855cb0b927978e775829786d7c227
cahyosubroto <cahyosubroto30@gmail.com>
2025-02-19 12:45:56 +0700

    feat: add filesystem protocol and revise http grpc kafka iamges

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -274,8 +274,8 @@ export const enSidebar = sidebar({
             },
             {
               text: "Filesystem",
-              collapsible: true,
-              children: ["filesystem/filesystem.md"],
+              link: "filesystem.md",
+              prefix: "concepts/protocol/",
             },
             {
               text: "TLS",
